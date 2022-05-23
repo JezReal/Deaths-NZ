a1df2d85e6322b55aaf6ab6133cdc9453260a018
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-05-23 23:01:20 +0800

    add graphs for data

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from data.data import import_data_to_database, get_total_deaths_yearly, get_tota
     get_total_deaths_by_year, get_total_male_deaths, get_total_deaths_by_age_group
 import matplotlib.pyplot as plot
 from ui import ui
+import numpy
 
 import_data_to_database()
-ui.run()
 
+
+# ui.run()
 
 # Main functionality
 def cli():
@@ -16,6 +18,7 @@ def cli():
     print('[c] Total female deaths from 2010-2021')
     print('[d] Total deaths by year')
     print('[e] Total deaths by age group')
+    print('[f] Total male and female deaths by year')
     data = input()
 
     match data:
@@ -44,12 +47,6 @@ def cli():
 
             deaths_count.append(deaths_sum)
 
-            print(years)
-            print(deaths_count)
-
-            print(len(years))
-            print(len(deaths_count))
-
             plot.plot(years, deaths_count)
             plot.title('Total deaths by year')
             plot.xlabel('Year')
@@ -59,19 +56,65 @@ def cli():
         case 'b':
             deaths = get_total_male_deaths()
             deaths_sum = 0
+            years = []
+            deaths_count = []
 
             for data in deaths:
-                deaths_sum += int(data['Count'])
+                current_year = data['Period']
+                current_death_count = data['Count']
 
-            print(f'Total male deaths from 2010-2021: {deaths_sum}')
+                try:
+                    if current_year != years[-1]:
+                        years.append(current_year)
+                        deaths_count.append(deaths_sum)
+                        deaths_sum = int(current_death_count)
+                    elif current_year == '2021':
+                        deaths_sum += int(current_death_count)
+                    else:
+                        deaths_sum += int(current_death_count)
+                except IndexError:
+                    years.append(current_year)
+                    deaths_sum = int(current_death_count)
+
+            deaths_count.append(deaths_sum)
+
+            plot.plot(years, deaths_count)
+            plot.title('Total male deaths by year')
+            plot.xlabel('Year')
+            plot.ylabel('Deaths')
+            plot.show()
+
         case 'c':
             deaths = get_total_female_deaths()
             deaths_sum = 0
+            years = []
+            deaths_count = []
 
             for data in deaths:
-                deaths_sum += int(data['Count'])
+                current_year = data['Period']
+                current_death_count = data['Count']
 
-            print(f'Total female deaths from 2010-2021: {deaths_sum}')
+                try:
+                    if current_year != years[-1]:
+                        years.append(current_year)
+                        deaths_count.append(deaths_sum)
+                        deaths_sum = int(current_death_count)
+                    elif current_year == '2021':
+                        deaths_sum += int(current_death_count)
+                    else:
+                        deaths_sum += int(current_death_count)
+                except IndexError:
+                    years.append(current_year)
+                    deaths_sum = int(current_death_count)
+
+            deaths_count.append(deaths_sum)
+
+            plot.plot(years, deaths_count)
+            plot.title('Total FEmale deaths by year')
+            plot.xlabel('Year')
+            plot.ylabel('Deaths')
+            plot.show()
+
         case 'd':
             input_year = input('Enter year: ')
 
@@ -82,6 +125,7 @@ def cli():
                 deaths_sum += int(data['Count'])
 
             print(f'Total deaths in {input_year}: {deaths_sum}')
+
         case 'e':
             input_age_group = input('Enter age group: ')
 
@@ -92,3 +136,65 @@ def cli():
                 deaths_sum += int(data['Count'])
 
             print(f'Total deaths in {input_age_group}: {deaths_sum}')
+
+        case 'f':
+            years = []
+            total_male_deaths = []
+            total_female_deaths = []
+
+            male_deaths_sum = 0
+            female_deaths_sum = 0
+            male_deaths = get_total_male_deaths()
+            female_deaths = get_total_female_deaths()
+
+            for death in male_deaths:
+                current_year = death['Period']
+                current_death_count = death['Count']
+
+                try:
+                    if current_year != years[-1]:
+                        years.append(current_year)
+                        total_male_deaths.append(male_deaths_sum)
+                        male_deaths_sum = int(current_death_count)
+                    elif current_year == '2021':
+                        male_deaths_sum += int(current_death_count)
+                    else:
+                        male_deaths_sum += int(current_death_count)
+                except IndexError:
+                    years.append(current_year)
+                    male_deaths_sum = int(current_death_count)
+
+            total_male_deaths.append(male_deaths_sum)
+
+            years = []
+            for death in female_deaths:
+                current_year = death['Period']
+                current_death_count = death['Count']
+
+                try:
+                    if current_year != years[-1]:
+                        years.append(current_year)
+                        total_female_deaths.append(female_deaths_sum)
+                        female_deaths_sum = int(current_death_count)
+                    elif current_year == '2021':
+                        female_deaths_sum += int(current_death_count)
+                    else:
+                        female_deaths_sum += int(current_death_count)
+                except IndexError:
+                    years.append(current_year)
+                    female_deaths_sum = int(current_death_count)
+
+            total_female_deaths.append(female_deaths_sum)
+
+            x_axis = numpy.arange(len(years))
+            plot.figure(figsize=(10, 5))
+            plot.bar(x_axis - 0.2, total_male_deaths, width=0.4, label='Male')
+            plot.bar(x_axis + 0.2, total_female_deaths, width=0.4, label='Female')
+
+            plot.title('Deaths of male and female per year')
+            plot.xticks(x_axis, years)
+            plot.legend()
+            plot.show()
+
+
+cli()
