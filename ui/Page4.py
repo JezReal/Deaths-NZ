84593a56d0a66827045e03bc1987ce0f3a4ea77b
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-05-31 22:16:54 +0800

    change options of option menu

--- ui/Page4.py
+++ ui/Page4.py
@@ -105,12 +105,17 @@ class Frame4(tk.Frame):
         )
         self.pack_propagate(0)
         self.language = (
-            'Infant', '1 Year old',
-            '2 Years old', '3 Years old',
-            '4 Years old', '5 Years old',
-            '6 Years old', '7 Years old',
-            '8 Years old', '9 Years old',
-            '10 Years old', '11 Years old'
+            'Infant', '1-4',
+            '5-9', '10-14',
+            '15-19', '20-24',
+            '25-29', '30-34',
+            '35-39', '40-44',
+            '45-49', '50-54',
+            '55-59', '60-64',
+            '65-69', '70-74',
+            '75-79', '80-84',
+            '85-89', '90-94',
+            '95-99', '100 and over'
         )
         self.option_var = tk.StringVar(self)
         self.create_wigets()
