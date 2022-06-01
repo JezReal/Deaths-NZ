fbbb7840b4fca3c86c680ba4494a704d66bdcc65
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-01 23:35:16 +0800

    implement getting total deaths by age group

--- ui/Page4.py
+++ ui/Page4.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-from data.data import get_total_female_deaths
+from data.data import get_total_deaths_by_age_group
+from ui.Page2 import bind_event_data
 
 spacing = 20
 FrameHeight = 100
@@ -30,20 +31,25 @@ class Page4(ttk.Frame):
         )
         titleLabel.grid(row=0, column=0, columnspan=3, pady=(0, spacing), )
 
-        frame2 = Frame2(self)
-        frame2.grid(column=0, row=1, padx=(0, spacing), pady=(0, spacing), sticky='nsew')
+        self.frame2 = Frame2(self)
+        self.frame2.grid(column=0, row=1, padx=(0, spacing), pady=(0, spacing), sticky='nsew')
 
         frame3 = Frame3(self)
         frame3.grid(column=1, row=1, padx=(0, spacing), pady=(0, spacing), sticky='nsew')
 
         frame4 = Frame4(self)
         frame4.grid(column=2, row=1, pady=(0, spacing), sticky='nsew')
+        bind_event_data(frame4, '<<AgeGroupSelectEvent>>', self.age_group_change_callback)
 
         frame1 = Frame1(self)
         frame1.grid(column=0, row=2, columnspan=3, sticky='nsew')
 
         self.columnconfigure(tuple(range(3)), weight=1)
         self.rowconfigure(tuple(range(3)), weight=1)
+
+    def age_group_change_callback(self, event):
+        selected_age_group = event.data['age_group']
+        self.frame2.update_death_count(selected_age_group)
 
 
 class Frame1(tk.Frame):
@@ -68,13 +74,17 @@ class Frame2(tk.Frame):
             width=FrameWidth,
             bg=color1,
         )
-        self.female_deaths_sum = get_total_deaths()
+        self.deaths_sum = get_total_deaths('Infant')
         self.pack_propagate(0)
 
-        label1 = tk.Label(self, text=self.female_deaths_sum, bg=color1, fg=color3, font='arial 35 bold')
-        label1.pack(expand=1, fill='both', pady=0)
+        self.label1 = tk.Label(self, text=self.deaths_sum, bg=color1, fg=color3, font='arial 35 bold')
+        self.label1.pack(expand=1, fill='both', pady=0)
         label2 = tk.Label(self, text='Total Deaths', bg=color1, font='arial 20 bold')
         label2.pack(expand=1, fill='both', )
+
+    def update_death_count(self, age_group):
+        print('Callback hit')
+        self.label1['text'] = get_total_deaths(age_group)
 
 
 class Frame3(tk.Frame):
@@ -103,6 +113,7 @@ class Frame4(tk.Frame):
             padx=20,
             pady=10
         )
+        self.option_menu = None
         self.pack_propagate(0)
         self.language = (
             'Infant', '1-4',
@@ -119,31 +130,47 @@ class Frame4(tk.Frame):
         )
         self.option_var = tk.StringVar(self)
         self.create_wigets()
+        self.selected_age_group = 'Infant'
+
+        self.option_var.trace_add(('write', 'unset'), self.create_virtual_event)
 
     def create_wigets(self):
         paddings = {'padx': 90, 'pady': 7}
 
-        option_menu = ttk.OptionMenu(
+        self.option_menu = ttk.OptionMenu(
             self,
             self.option_var,
             self.language[0],
-            *self.language,
-            command=self.option_changed)
+            *self.language)
 
-        option_menu.pack(fill='x', expand=1, )
+        self.option_menu.pack(fill='x', expand=1)
+        self.option_menu.bind('<<OptionMenuChanged>>', self.select_callback)
 
         label = tk.Label(self, text='Age', bg=color1, font='arial 20 bold')
         label.pack()
 
-    def option_changed(self, *args):
-        pass
+    def create_virtual_event(self, varname, idx, mode):
+        self.option_menu.event_generate('<<OptionMenuChanged>>')
+        self.generate_age_group_change_event()
+
+    def select_callback(self, event):
+        self.selected_age_group = self.option_var.get()
+        self.generate_age_group_change_event()
+
+    def generate_age_group_change_event(self):
+        self.event_generate('<<AgeGroupSelectEvent>>', data={'age_group': self.selected_age_group})
 
 
-def get_total_deaths():
-    deaths = get_total_female_deaths()
+def get_total_deaths(age_group):
     deaths_sum = 0
+
+    deaths = get_total_deaths_by_age_group(age_group)
 
     for death in deaths:
         deaths_sum += int(death['Count'])
 
     return deaths_sum
+
+
+def get_graph_data(age_group):
+    pass
